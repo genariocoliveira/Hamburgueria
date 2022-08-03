54bbe2ec8e6eb527d5856865bc1848c579afdf6e
Genário Carneiro <gennaroplay.17@gmail.com>
2022-08-03 09:42:04 -0300

    primeiro commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="footer">
+        <div class="main-footer">
+            <p>Todos os direitos reservados &copy; 2022</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Footer'
+}
+</script>
+
+<style scoped>
+.footer{
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+}
+.main-footer{
+    height: 50px;
+    padding: 20px 0;
+    background: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+}
+
+</style>
